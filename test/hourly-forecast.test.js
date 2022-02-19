808d2749600b08aa3bb044b9e5ac94281505c7f3
rsdiaz <robertoserrano83@gmail.com>
2022-02-19 23:44:24 +0100

    New test for hourly forecast

---
+++ test/hourly-forecast.test.js
@@ -0,0 +1,7 @@
+/* eslint-disable no-undef */
+import { HourlyForecast } from '../lib/hourly-forecast';
+
+const apiKey = 'YOUR API KEY';
+const hourlyForecast = new HourlyForecast(apiKey);
+
+describe('');
